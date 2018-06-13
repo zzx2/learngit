bf64a904d2e2cb353e17d33b89a6316b7afb494a
zzx2 <781914164@qq.com>
2018-06-13 16:49:44 +0800

    icons

---
+++ trproject/src/pages/home/components/Icons.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="icons">
+   <swiper :options="swiperOption">
+    <swiper-slide v-for="(page,index) in pages" :key="index">
+      <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+             <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-font">{{item.icontent}}</p>
+      </div>
+    </swiper-slide>
+   </swiper>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'Icons',
+  data () {
+    return {
+      swiperOption: {
+        autoplayDisableOnInteraction: false
+      },
+      IconsList: [
+        {
+          id: '001',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '002',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '003',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '004',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '005',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '006',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '007',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '008',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        },
+        {
+          id: '009',
+          imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3537084900,3800051897&fm=58',
+          icontent: '电玩游戏'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages: function () {
+      const pages = []
+      this.IconsList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixin.styl'
+  .icons
+     overflow :hidden
+     height :0
+     padding-bottom :50%
+     .icon
+      position :relative
+      float :left
+      width :25%
+      height :0
+      padding-bottom:25%
+      .icon-img
+       position :absolute
+       top :0
+       left :0
+       right :0
+       bottom :0.44rem
+       box-sizing :border-box
+       padding :0.1rem
+       .icon-img-content
+          display :block
+          margin : 0 auto
+          height :100%
+      .icon-font
+         position :absolute
+         left:0
+         right:0
+         bottom :0
+         line-height :0.44rem
+         text-align :center
+         color :#333333
+         ellips()
+</style>
--- trproject/src/pages/home/home.vue
+++ trproject/src/pages/home/home.vue
@@ -2,17 +2,19 @@
     <div>
         <Header />
         <Homeswper />
-        test
+        <Icons />
     </div>
 </template>
 <script>
 import Header from './components/header'
 import Homeswper from './components/Homeswper'
+import Icons from './components/Icons'
 export default {
   name: 'home',
   components: {
     Header,
-    Homeswper
+    Homeswper,
+    Icons
   }
 }
 </script>
