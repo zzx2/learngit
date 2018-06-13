cd7ec51c49518b9ed58da92bfb1923548f7d35bd
zzx2 <781914164@qq.com>
2018-06-13 17:23:31 +0800

    Recommend

---
+++ trproject/src/pages/home/components/Hrecommend.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+      <p class="retitle">推荐景点</p>
+   </div>
+
+</template>
+<script>
+export default {
+  name: 'Hrecommend',
+  data () {
+    return {
+
+    }
+  }
+
+}
+</script>
+<style lang='stylus' scoped>
+  .retitle
+     line-height :0.66rem
+     background :#cccc
+
+</style>
--- trproject/src/pages/home/home.vue
+++ trproject/src/pages/home/home.vue
@@ -3,18 +3,21 @@
         <Header />
         <Homeswper />
         <Icons />
+        <Hrecommend />
     </div>
 </template>
 <script>
 import Header from './components/header'
 import Homeswper from './components/Homeswper'
 import Icons from './components/Icons'
+import Hrecommend from './components/Hrecommend'
 export default {
   name: 'home',
   components: {
     Header,
     Homeswper,
-    Icons
+    Icons,
+    Hrecommend
   }
 }
 </script>
